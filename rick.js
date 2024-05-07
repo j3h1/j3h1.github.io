e71072e71071443ea4306f61442af4e86a467191
j3h1 <169068827+j3h1@users.noreply.github.com>
2024-05-07 17:13:25 +0800

    Create rick.js

---
+++ rick.js
@@ -0,0 +1,2 @@
+var im = document.getElementById("stupic");
+im.parentNode.innerHTML = '<iframe width='+im.width+' height='+im.height+' src="https://www.youtube.com/embed/dQw4w9WgXcQ?si=Dq6gS5nYunrmw1JO&autoplay=1&controls=0&showinfo=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>';
